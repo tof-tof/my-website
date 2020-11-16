953ca5787968d0450122e3d5fa9f3e2b7c5ce60c
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2020-11-16 01:16:47 +0000

    no backgrounds, just blob

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hover from './Hover';
 import Goo from './Blob';
 
 
-
 function App() {
   const headerProps = useSpring({
     opacity: 1,
@@ -15,10 +14,9 @@ function App() {
   
   return (
     <div>
-      <Goo/>
+      <Goo />
       <div>
         <img className="round-img" src="../tof.jpg" alt=" Tofunmi Adigun-Hameed" />
-        
       </div>
       <animated.h1 style={headerProps}>hello!</animated.h1>
       <h3>I'm Tofunmi Adigun-Hameed (aka Tof), an I'm an aspiring <i>Software Engineer</i> &#128522;</h3>
@@ -31,7 +29,6 @@ function App() {
       <Hover href={"https://www.linkedin.com/in/tofunmi-adigun-hameed/"} src={"../linkedin.png"} alt={"linkedIn"} />
       <div className="no-line-break pad-out"></div>
       <Hover href={""} src={"../profile.png"} alt={"cv"} />
-      
     </div>
   );
 }
