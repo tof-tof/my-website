95109b9db1fd724b9d1a29ae79279ab5a8ddc0e9
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2020-11-17 12:57:35 +0000

    graphics completed black -white style

--- src/App.js
+++ src/App.js
@@ -16,46 +16,73 @@ function App() {
   
   return (
     <div>
-      <div>
-        <Particles
-          style={{ position: "absolute" }}
-          height="95%"
-          width="30%"
-          params={{
-            particles: {
+      <Particles
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0}}
+        height="70%"
+        width="37%"
+        params={{
+          particles: {
+            color: {
+              value: "#000000"
+            },
+            line_linked: {
               color: {
                 value: "#000000"
-              },
-              line_linked: {
-                color: {
-                  value: "#000000"
-                }
-              },
-              number: {
-                value: 50
-              },
-              size: {
-                value: 3
               }
+            },
+            number: {
+              value: 50
+            },
+            size: {
+              value: 3
             }
-          }}
-          />
-      </div>
-      <div>
-        <img className="round-img" src="../tof.jpg" alt=" Tofunmi Adigun-Hameed" />
-      </div>
-      <animated.h1 style={headerProps}>Hello!</animated.h1>
-      <h3>I'm Tofunmi Adigun-Hameed (aka Tof), an I'm an aspiring <i>Software Engineer</i> &#128522;</h3>
-      <p className="space-after">
-        I'm currently studying Mathematics and Computer Science at the University of Oxford and I love learning about and working with new technologies.
-        My other passions include outreach and Zumba.
-    </p>
-      <Hover href={"https://github.com/tof-tof"} src={"../github.png"} alt={"github"} />
-      <div className="no-line-break pad-out"></div>
-      <Hover href={"https://www.linkedin.com/in/tofunmi-adigun-hameed/"} src={"../linkedin.png"} alt={"linkedIn"} />
-      <div className="no-line-break pad-out"></div>
-      <Hover href={Pdf} target ={"_blank"} src={"../profile.png"} alt={"cv"} />
+          }
+        }}
+      />
+      <Particles
+        style={{
+          position: "fixed",
+          top: 0,
+          right: 0}}
+        height="70%"
+        width="37%"
+        params={{
+          particles: {
+            color: {
+              value: "#000000"
+            },
+            line_linked: {
+              color: {
+                value: "#000000"
+              }
+            },
+            number: {
+              value: 50
+            },
+            size: {
+              value: 3
+            }
+          }
+        }}
+    />
+    <div>
+    <img className="round-img" src="../tof.jpg" alt=" Tofunmi Adigun-Hameed" />
     </div>
+    <animated.h1 style={headerProps}>Hello!</animated.h1>
+    <h3>I'm Tofunmi Adigun-Hameed (aka Tof), an I'm an aspiring <i>Software Engineer</i> &#128522;</h3>
+    <p className="space-after">
+      I'm currently studying Mathematics and Computer Science at the University of Oxford and I love learning about and working with new technologies.
+      My other passions include outreach and Zumba.
+    </p>
+    <Hover href={"https://github.com/tof-tof"} src={"../github.png"} alt={"github"} />
+    <div className="no-line-break pad-out"></div>
+    <Hover href={"https://www.linkedin.com/in/tofunmi-adigun-hameed/"} src={"../linkedin.png"} alt={"linkedIn"} />
+    <div className="no-line-break pad-out"></div>
+    <Hover href={Pdf} target={"_blank"} src={"../profile.png"} alt={"cv"} /> 
+  </div>
   );
 }
 
