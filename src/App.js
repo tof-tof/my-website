4cf63c22be7fd1b70eb8ebdf810376d7d98244d6
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2020-11-17 12:25:41 +0000

    added particles - need to be moved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
 import { useSpring, animated } from 'react-spring';
-import AnimatedPic from './AnimatedPic';
-import Goo from './Blob';
-import Pdf from './assets/Adigun-Hameed_T.pdf' 
+import Hover from './AnimatedPic';
+//import Goo from './Blob';
+import Pdf from './assets/Adigun-Hameed_T.pdf';
+import Particles from 'react-particles-js';
+
 
 
 function App() {
@@ -15,7 +16,31 @@ function App() {
   
   return (
     <div>
-      <Goo />
+      <div>
+        <Particles
+          style={{ position: "absolute" }}
+          height="95%"
+          width="30%"
+          params={{
+            particles: {
+              color: {
+                value: "#000000"
+              },
+              line_linked: {
+                color: {
+                  value: "#000000"
+                }
+              },
+              number: {
+                value: 50
+              },
+              size: {
+                value: 3
+              }
+            }
+          }}
+          />
+      </div>
       <div>
         <img className="round-img" src="../tof.jpg" alt=" Tofunmi Adigun-Hameed" />
       </div>
@@ -25,11 +50,11 @@ function App() {
         I'm currently studying Mathematics and Computer Science at the University of Oxford and I love learning about and working with new technologies.
         My other passions include outreach and Zumba.
     </p>
-      <AnimatedPic href={"https://github.com/tof-tof"} src={"../github.png"} alt={"github"} />
+      <Hover href={"https://github.com/tof-tof"} src={"../github.png"} alt={"github"} />
       <div className="no-line-break pad-out"></div>
-      <AnimatedPic href={"https://www.linkedin.com/in/tofunmi-adigun-hameed/"} src={"../linkedin.png"} alt={"linkedIn"} />
+      <Hover href={"https://www.linkedin.com/in/tofunmi-adigun-hameed/"} src={"../linkedin.png"} alt={"linkedIn"} />
       <div className="no-line-break pad-out"></div>
-      <AnimatedPic href={Pdf} target ={"_blank"} src={"../profile.png"} alt={"cv"} />
+      <Hover href={Pdf} target ={"_blank"} src={"../profile.png"} alt={"cv"} />
     </div>
   );
 }
