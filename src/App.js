cd711542f4f0f7f236a987796002bef82c01a452
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-02-25 18:01:06 +0000

    fixed typo

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <ParticleComponent />
     <img className="round-img" src="../tof.jpg" alt=" Tofunmi Adigun-Hameed" />
     <animated.h1 style={headerProps}>Hello!</animated.h1>
-    <h3>I'm Tofunmi Adigun-Hameed (aka Tof), an I'm an aspiring <i style={{color: 'blue'}}>Software Engineer</i> &#128522;</h3>
+    <h3>I'm Tofunmi Adigun-Hameed (aka Tof), and I'm an aspiring <i style={{color: 'blue'}}>Software Engineer</i> &#128522;</h3>
     <p className="space-after">
       I'm currently studying Mathematics and Computer Science at the University of Oxford and I love learning about and working with new technologies.
       My other passions include outreach and Zumba.
